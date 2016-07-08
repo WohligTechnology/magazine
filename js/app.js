264c805737c993db3c7543a168204f8d72bcb1ae
sohan1 <shn619@gmail.com>
2016-07-08 19:34:03 +0530

    animation

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 // JavaScript Document
+var ZoomValue = 1;
+var HeightChange = 1;
+var controller = new ScrollMagic.Controller();
+var Section8Height = 800;
 var firstapp = angular.module('firstapp', [
   'ui.router',
   'phonecatControllers',
@@ -42,6 +46,67 @@ firstapp.config(function($stateProvider, $urlRouterProvider, $httpProvider, $loc
   $locationProvider.html5Mode(isproduction);
 });
 
+firstapp.directive('ngScrollDuration', function($compile, $parse) {
+  return {
+    restrict: 'EA',
+    replace: false,
+    link: function($scope, element, attrs) {
+      var $element = $(element);
+      var animation = {};
+      console.log("NGSCROLL", attrs);
+      var animationSceneFrom = eval("(" + attrs.ngScrollAnimationFrom + ")");
+      var animationSceneTo = eval("(" + attrs.ngScrollAnimationTo + ")");
+      var id = attrs.id;
+      var pin = attrs.pin;
+      var toggleClass = attrs.toggleClass;
+      if (attrs.ngScrollTrigger) {
+        animation.triggerElement = attrs.pin;
+      }
+      animation.duration = attrs.ngScrollDuration * ZoomValue;
+      animation.offset = attrs.ngScrollOffset * ZoomValue;
+      // build twee
+      var wipeAnimation = TweenMax.fromTo($element, 1, animationSceneFrom, animationSceneTo);
+
+      // build scene
+      if (pin) {
+        var scene = new ScrollMagic.Scene(animation)
+          .setTween(wipeAnimation )
+          .setPin(pin, {
+            pushFollowers: false
+          })
+          .addIndicators() // add indicators (requires plugin)
+          .addTo(controller);
+      } else {
+        var scene = new ScrollMagic.Scene(animation)
+          .setTween(wipeAnimation)
+          .addIndicators() // add indicators (requires plugin)
+          .addTo(controller);
+      }
+    }
+  };
+});
+
+// var wipeAnimation = new TimelineMax().fromTo("section.two", 1, {x: "-100%"}, {x: "0%",ease: Linear.easeNone}) // in from left
+// .fromTo("section.three", 1, {x: "100%"}, {x: "0%",ease: Linear.easeNone}) // in from right
+// .fromTo("section.four", 1, {y: "-100%"}, {y: "0%",ease: Linear.easeNone}); // in from
+//
+// firstapp.directive('markanimation', function($compile, $parse) {
+//   return {
+//     restrict: 'EA',
+//     replace: false,
+//     link: function($scope, element, attrs) {
+//       new ScrollMagic.Scene({
+//           triggerElement: "#pinContainer",
+//           triggerHook: "onLeave",
+//           duration: "300%"
+//         })
+//         .setPin("#pinContainer")
+//         .setTween(wipeAnimation)
+//         .addIndicators() // add indicators (requires plugin)
+//         .addTo(controller)
+//     }
+//   };
+// });
 
 firstapp.directive('img', function($compile, $parse) {
   return {
